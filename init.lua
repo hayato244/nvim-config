70ec5817fa19bc72671c5d64c8a729257a9aa8f7
lucas <lutz.rengsdorf@web.de>
2026-05-30 23:43:52 +0200

    added conform and gitsigns

--- init.lua
+++ init.lua
@@ -103,10 +103,12 @@ vim.pack.add({
 		src = gh 'nvim-treesitter/nvim-treesitter',
 		version = 'main'
 	},
+	gh 'lewis6991/gitsigns.nvim',
+	gh 'stevearc/conform.nvim'
 })
 
 vim.pack.add(telescope_plugins)
-	
+
 require("oil").setup({})
 
 require("mini.statusline").setup({
@@ -247,3 +249,47 @@ end, { desc = 'Search in Open Files' })
 
 -- Shortcut for searching your Neovim configuration files
 vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = 'Search Neovim files' })
+
+require('gitsigns').setup({
+	signs = {
+		add = { text = '+' },
+		change = { text = '~' },
+		delete = { text = '_' },
+		topdelete = { text = '‾' },
+		changedelete = { text = '~' },
+	}
+})
+
+require('conform').setup {
+	notify_on_error = false,
+	format_on_save = function(bufnr)
+		-- You can specify filetypes to autoformat on save here:
+		local enabled_filetypes = {
+		-- lua = true,
+		-- python = true,
+		}
+		if enabled_filetypes[vim.bo[bufnr].filetype] then
+			return { timeout_ms = 500 }
+		else
+			return nil
+		end
+	end,
+	default_format_opts = {
+		lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
+	},
+
+	-- You can also specify external formatters in here.
+	formatters_by_ft = {
+		-- rust = { 'rustfmt' },
+		-- Conform can also run multiple formatters sequentially
+		-- python = { "isort", "black" },
+		--
+		-- You can use 'stop_after_first' to run the first available formatter from the list
+		-- javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+}
+
+vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
+	require('conform').format { async = true }
+end, { desc = 'Format buffer' })
+
